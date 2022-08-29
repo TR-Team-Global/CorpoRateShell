1354a598d699007bc11d714df12d1fbafb908808
TR-Team-Global <111459792+TR-Team-Global@users.noreply.github.com>
2022-08-29 15:48:36 +0530

    Add files via upload

---
+++ comp.ps1
@@ -0,0 +1,281 @@
+
+filter domain {
+
+
+    param(
+        [Parameter(ValueFromPipeline=$True)]
+        [String]
+        $Domain,
+
+        [Management.Automation.PSCredential]
+        $Credential
+    )
+
+    if($Credential) {
+        
+        Write-Verbose "Using alternate credentials for domain"
+
+        if(!$Domain) {
+            # if no domain is supplied, extract the logon domain from the PSCredential passed
+            $Domain = $Credential.GetNetworkCredential().Domain
+            Write-Verbose "Extracted domain '$Domain' from -Credential"
+        }
+   
+        $DomainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Domain', $Domain, $Credential.UserName, $Credential.GetNetworkCredential().Password)
+        
+        try {
+            [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($DomainContext)
+        }
+        catch {
+            Write-Verbose "The specified domain does '$Domain' not exist, could not be contacted, there isn't an existing trust, or the specified credentials are invalid."
+            $Null
+        }
+    }
+    elseif($Domain) {
+        $DomainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Domain', $Domain)
+        try {
+            [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($DomainContext)
+        }
+        catch {
+            Write-Verbose "The specified domain '$Domain' does not exist, could not be contacted, or there isn't an existing trust."
+            $Null
+        }
+    }
+    else {
+        [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+    }
+}
+
+
+filter domainsearcher {
+
+
+    param(
+        [Parameter(ValueFromPipeline=$True)]
+        [String]
+        $Domain,
+
+        [String]
+        $DomainController,
+
+        [String]
+        $ADSpath,
+
+        [String]
+        $ADSprefix,
+
+        [ValidateRange(1,10000)] 
+        [Int]
+        $PageSize = 200,
+
+        [Management.Automation.PSCredential]
+        $Credential
+    )
+
+    if(-not $Credential) {
+        if(-not $Domain) {
+            $Domain = (domain).name
+        }
+        elseif(-not $DomainController) {
+            try {
+                # if there's no -DomainController specified, try to pull the primary DC to reflect queries through
+                $DomainController = ((domain).PdcRoleOwner).Name
+            }
+            catch {
+                throw "domainsearcher: Error in retrieving PDC for current domain"
+            }
+        }
+    }
+    elseif (-not $DomainController) {
+        # if a DC isn't specified
+        try {
+            $DomainController = ((domain -Credential $Credential).PdcRoleOwner).Name
+        }
+        catch {
+            throw "domainsearcher: Error in retrieving PDC for current domain"
+        }
+
+        if(!$DomainController) {
+            throw "domainsearcher: Error in retrieving PDC for current domain"
+        }
+    }
+
+    $SearchString = "LDAP://"
+
+    if($DomainController) {
+        $SearchString += $DomainController
+        if($Domain){
+            $SearchString += '/'
+        }
+    }
+
+    if($ADSprefix) {
+        $SearchString += $ADSprefix + ','
+    }
+
+    if($ADSpath) {
+        if($ADSpath -Match '^GC://') {
+            # if we're searching the global catalog
+            $DN = $AdsPath.ToUpper().Trim('/')
+            $SearchString = ''
+        }
+        else {
+            if($ADSpath -match '^LDAP://') {
+                if($ADSpath -match "LDAP://.+/.+") {
+                    $SearchString = ''
+                }
+                else {
+                    $ADSpath = $ADSpath.Substring(7)
+                }
+            }
+            $DN = $ADSpath
+        }
+    }
+    else {
+        if($Domain -and ($Domain.Trim() -ne "")) {
+            $DN = "DC=$($Domain.Replace('.', ',DC='))"
+        }
+    }
+
+    $SearchString += $DN
+    Write-Verbose "domainsearcher search string: $SearchString"
+
+    if($Credential) {
+        Write-Verbose "Using alternate credentials for LDAP connection"
+        $DomainObject = New-Object DirectoryServices.DirectoryEntry($SearchString, $Credential.UserName, $Credential.GetNetworkCredential().Password)
+        $Searcher = New-Object System.DirectoryServices.DirectorySearcher($DomainObject)
+    }
+    else {
+        $Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+    }
+
+    $Searcher.PageSize = $PageSize
+    $Searcher.CacheResults = $False
+    $Searcher
+}
+
+function Computer {
+
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(ValueFromPipeline=$True)]
+        [Alias('HostName')]
+        [String]
+        $ComputerName = '*',
+
+        [String]
+        $SPN,
+
+        [String]
+        $OperatingSystem,
+
+        [String]
+        $ServicePack,
+
+        [String]
+        $Filter,
+
+        [Switch]
+        $Printers,
+
+        [Switch]
+        $Ping,
+
+        [Switch]
+        $FullData,
+
+        [String]
+        $Domain,
+
+        [String]
+        $DomainController,
+
+        [String]
+        $ADSpath,
+
+        [String]
+        $SiteName,
+
+        [Switch]
+        $Unconstrained,
+
+        [ValidateRange(1,10000)] 
+        [Int]
+        $PageSize = 200,
+
+        [Management.Automation.PSCredential]
+        $Credential
+    )
+
+    begin {
+        # so this isn't repeated if multiple computer names are passed on the pipeline
+        $CompSearcher = domainsearcher -Domain $Domain -DomainController $DomainController -ADSpath $ADSpath -PageSize $PageSize -Credential $Credential
+    }
+
+    process {
+
+        if ($CompSearcher) {
+
+            # if we're checking for unconstrained delegation
+            if($Unconstrained) {
+                Write-Verbose "Searching for computers with for unconstrained delegation"
+                $Filter += "(userAccountControl:1.2.840.113556.1.4.803:=524288)"
+            }
+            # set the filters for the seracher if it exists
+            if($Printers) {
+                Write-Verbose "Searching for printers"
+                # $CompSearcher.filter="(&(objectCategory=printQueue)$Filter)"
+                $Filter += "(objectCategory=printQueue)"
+            }
+            if($SPN) {
+                Write-Verbose "Searching for computers with SPN: $SPN"
+                $Filter += "(servicePrincipalName=$SPN)"
+            }
+            if($OperatingSystem) {
+                $Filter += "(operatingsystem=$OperatingSystem)"
+            }
+            if($ServicePack) {
+                $Filter += "(operatingsystemservicepack=$ServicePack)"
+            }
+            if($SiteName) {
+                $Filter += "(serverreferencebl=$SiteName)"
+            }
+
+            $CompFilter = "(&(sAMAccountType=805306369)(dnshostname=$ComputerName)$Filter)"
+            Write-Verbose "Computer filter : '$CompFilter'"
+            $CompSearcher.filter = $CompFilter
+
+            try {
+                $Results = $CompSearcher.FindAll()
+                $Results | Where-Object {$_} | ForEach-Object {
+                    $Up = $True
+                    if($Ping) {
+                        # TODO: how can these results be piped to ping for a speedup?
+                        $Up = Test-Connection -Count 1 -Quiet -ComputerName $_.properties.dnshostname
+                    }
+                    if($Up) {
+                        # return full data objects
+                        if ($FullData) {
+                            # convert/process the LDAP fields for each result
+                            $Computer = Convert-LDAPProperty -Properties $_.Properties
+                            $Computer.PSObject.TypeNames.Add('PowerView.Computer')
+                            $Computer
+                        }
+                        else {
+                            # otherwise we're just returning the DNS host name
+                            $_.properties.dnshostname
+                        }
+                    }
+                }
+                $Results.dispose()
+                $CompSearcher.dispose()
+            }
+            catch {
+                Write-Warning "Error: $_"
+            }
+        }
+    }
+}
+
+
